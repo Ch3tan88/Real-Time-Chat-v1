4785fb0703dae191c5a8028f79b08ffe361e9d7f
unknown <Ch3tan88>
2023-11-20 05:15:43 +0530

    checked..

--- RealTime-chat/vite.config.js
+++ RealTime-chat/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@mui/icons-material': '@mui/icons-material/Menu', // Example alias
+    },
+  },
+});
