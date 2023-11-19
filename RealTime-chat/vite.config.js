c28e4156101a6219455ced76134096958de4a363
unknown <Ch3tan88>
2023-11-20 05:29:11 +0530

    created appbar

--- RealTime-chat/vite.config.js
+++ RealTime-chat/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@mui/icons-material': '@mui/icons-material/Menu', // Example alias
-    },
-  },
-});
+  plugins: [react()],
+})
