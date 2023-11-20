fe3b20fcbc6081bed42d45ca0e605c18c71d21f7
unknown <Ch3tan88>
2023-11-20 06:05:51 +0530

    check 2

--- RealTime-chat/src/main.jsx
+++ RealTime-chat/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Appbar.jsx'
+import Appbar from './Appbar'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Appbar/>
   </React.StrictMode>,
 )
+
+
